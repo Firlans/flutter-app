f471be00194b968a30a6d0bb3c032d2d5a201a0f
firlans <firlansyah54321@gmail.com>
2024-07-03 13:24:02 +0700

    project flutter app UAS start

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/usr/bin/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/mrtech/Documents/flutter-app"
+export "FLUTTER_APPLICATION_PATH=/home/mrtech/Documents/flutter_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
