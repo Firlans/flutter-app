f92d822d7b307df5528622a8b63d735a746d6ae2
raffiMRG <mutekinoraffi@gmail.com>
2024-07-05 18:55:25 +0700

    new dat

--- models/mahasiswa/mahasiswa.go
+++ models/mahasiswa/mahasiswa.go
@@ -8,10 +8,13 @@ type Mahasiswa struct {
 	IdTa        int    `gorm:"column:id_ta"`
 	Nim         string `gorm:"column:nim;type:char(12)"`
 	NamaLengkap string `gorm:"column:nama_lengkap;type:varchar(255)"`
-	NamaOrtu    string `gorm:"column:nama_ortu;type:varchar(255)"`
-	Telp        string `gorm:"column:telp;type:char(15)"`
-	Foto        []byte `gorm:"column:foto;type:blob"`
-	Status      string `gorm:"column:status;type:enum('AKTIF','TIDAK AKTIF')"`
+	// TanggalLahir time.Time `gorm:"column:tanggal_lahir;type:date"`
+	TanggalLahir string `gorm:"column:tanggal_lahir;"`
+	Alamat       string `gorm:"column:alamat;type:varchar(255)"`
+	NamaOrtu     string `gorm:"column:nama_ortu;type:varchar(255)"`
+	Telp         string `gorm:"column:telp;type:char(15)"`
+	Foto         string `gorm:"column:foto;type:varchar(255)"`
+	Status       string `gorm:"column:status;type:enum('AKTIF','TIDAK AKTIF')"`
 }
 
 func (Mahasiswa) TableName() string {
